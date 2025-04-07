ddd9a0576cf15822295e3d6a3f2f5f52247da194
YaroslavKSE <ikhomych@kse.org.ua>
2025-04-07 16:28:58 +0300

    Create /new-releases endpoint which fetched new releases for Home page

---
+++ backend/MusicCatalogService/MusicCatalogService.Core/DTOs/NewReleasesResultDto.cs
@@ -0,0 +1,11 @@
+﻿namespace MusicCatalogService.Core.DTOs;
+
+public class NewReleasesResultDto
+{
+    public int Limit { get; set; }
+    public int Offset { get; set; }
+    public int TotalResults { get; set; }
+    public string Next { get; set; }
+    public string Previous { get; set; }
+    public List<AlbumSummaryDto> Albums { get; set; } = new();
+}
