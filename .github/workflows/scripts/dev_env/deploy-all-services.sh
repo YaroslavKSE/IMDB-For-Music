b731623d2cdf621fcd02fd25a60d87faf6c56a25
YaroslavKSE <ikhomych@kse.org.ua>
2025-04-09 13:44:58 +0300

    Simplified cd workflow

--- .github/workflows/scripts/dev_env/deploy-all-services.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ${DEPLOY_DIR}
-echo "Stopping all services"
-docker compose down
-echo "Pulling latest images"
-docker compose pull
-echo "Starting all services"
-docker compose up -d
-echo "Deployment completed for all services"
--- .github/workflows/scripts/dev_env/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ${DEPLOY_DIR}
-echo "Stopping service: ${SERVICE}"
-docker compose down ${SERVICE}
-echo "Pulling latest image for: ${SERVICE}"
-docker compose pull ${SERVICE}
-echo "Starting service: ${SERVICE}"
-docker compose up -d ${SERVICE}
-echo "Deployment completed for: ${SERVICE}"
